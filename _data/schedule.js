bf728bd39aa727ee36924e0169cc841fc6fd2e96
Saptak S <saptak013@gmail.com>
2022-12-08 19:30:34 +0530

    Adds template for schedule

---
+++ _data/schedule.js
@@ -0,0 +1,92 @@
+module.exports = [
+    {
+        "key": "day0",
+        "name": "Day 0, 23rd Dec.",
+        "schedule": [
+            {
+                "time": "10:00 AM",
+                "description": "Morning breakfast followed by some really big line that tests out the grid extent"
+            },
+            {
+                "time": "12:00 AM",
+                "description": "Morning breakfast"
+            },
+            {
+                "time": "2:00 AM",
+                "description": "Morning breakfast"
+            },
+            {
+                "time": "10:00 AM",
+                "description": "Morning breakfast"
+            },
+            {
+                "time": "10:00 AM",
+                "description": "Morning breakfast"
+            },
+            {
+                "time": "10:00 AM",
+                "description": "Morning breakfast"
+            },
+        ]
+    },
+    {
+        "key": "day1",
+        "name": "Day 1, 24th Dec.",
+        "schedule": [
+            {
+                "time": "10:00 AM",
+                "description": "Morning breakfast"
+            },
+            {
+                "time": "10:00 AM",
+                "description": "Morning breakfast"
+            },
+            {
+                "time": "10:00 AM",
+                "description": "Morning breakfast"
+            },
+            {
+                "time": "10:00 AM",
+                "description": "Morning breakfast"
+            },
+            {
+                "time": "10:00 AM",
+                "description": "Morning breakfast"
+            },
+            {
+                "time": "10:00 AM",
+                "description": "Morning breakfast"
+            },
+        ]
+    },
+    {
+        "key": "day2",
+        "name": "Day 2, 25th Dec.",
+        "schedule": [
+            {
+                "time": "10:00 AM",
+                "description": "Morning breakfast"
+            },
+            {
+                "time": "10:00 AM",
+                "description": "Morning breakfast"
+            },
+            {
+                "time": "10:00 AM",
+                "description": "Morning breakfast"
+            },
+            {
+                "time": "10:00 AM",
+                "description": "Morning breakfast"
+            },
+            {
+                "time": "10:00 AM",
+                "description": "Morning breakfast"
+            },
+            {
+                "time": "10:00 AM",
+                "description": "Morning breakfast"
+            },
+        ]
+    },
+];
