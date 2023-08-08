e741e943601c2f79817b84bf8ce32e94ee60d825
Saptak S <saptak013@gmail.com>
2023-08-08 16:06:16 +0530

    Adds basic layout for 2023

--- _data/mainnav.js
+++ _data/mainnav.js
@@ -1,23 +1,23 @@
 module.exports = [
-    {
-        "name": "About",
-        "url": "/#about"
-    },
-    {
-        "name": "Schedule",
-        "url": "/schedule"
-    },
-    {
-        "name": "Sponsors",
-        "url": "/#sponsors"
-    },
+    // {
+    //     "name": "About",
+    //     "url": "/#about"
+    // },
+    // {
+    //     "name": "Schedule",
+    //     "url": "/schedule"
+    // },
+    // {
+    //     "name": "Sponsors",
+    //     "url": "/#sponsors"
+    // },
     {
         "name": "CoC",
         "url": "/coc/"
     },
-    {
-        "name": "Register",
-        "url": "https://shdindia.myinstamojo.com/",
-        "type": "primary",
-    },
+    // {
+    //     "name": "Register",
+    //     "url": "https://shdindia.myinstamojo.com/",
+    //     "type": "primary",
+    // },
 ];
