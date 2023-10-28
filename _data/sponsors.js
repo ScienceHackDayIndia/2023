2e983415cfdda9cf9b0fa4cf35a31c856194fa29
rajudev <rajudev@disroot.org>
2023-10-28 15:14:02 +0530

    Added more community sponsors  New community sponsors added for   - Hotel New Uday Bhavan   - Super Castings   - Informant networks   there logo files added to assets.

--- _data/sponsors.js
+++ _data/sponsors.js
@@ -39,4 +39,14 @@ module.exports = [
         "image": "/assets/images/sponsors/pybelgaum.png",
         "type": "community",
     },
+    {
+        "name": "Hotel New Uday Bhuvan",
+        "image": "/assets/images/sponsors/ub.jpg",
+        "type": "community",
+    },
+    {
+        "name": "Super Castings",
+        "image": "/assets/images/sponsors/supercast.jpg",
+        "type": "community",
+    },   
 ];
