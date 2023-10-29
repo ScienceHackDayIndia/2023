b23d6a2a289ae72929216a2b0cd5d45aade3c961
Saptak S <saptak013@gmail.com>
2023-10-29 18:48:35 +0530

    Adds more project info

--- _data/projects.js
+++ _data/projects.js
@@ -32,7 +32,7 @@ module.exports = [
   {
       "title": "Harmonic Oscillator",
       "description": "",
-      "creator": "Praveen & Team",
+      "creator": "Jithin, Ishaan and Praveen",
       "contact": "",
   },
   {
@@ -45,6 +45,30 @@ module.exports = [
       "title": "Plasma vortex",
       "description": "",
       "creator": "Prathmesh & Team",
+      "contact": "",
+  },
+  {
+      "title": "Health Vigilance",
+      "description": "Built for patients and elders to raise an alert by monitoring heart rate using bangle JS",
+      "creator": "Shabnam, Farhaan, Shekhar",
+      "contact": "",
+  },
+  {
+      "title": "Night vision driving improvement",
+      "description": "Nigh vision driving improvement system using webcam and LCD screen to help drivers avoid bright light during driving",
+      "creator": "Anant & Team",
+      "contact": "",
+  },
+  {
+      "title": "LLM Resume Search",
+      "description": "Based on distilled bert which is a language transformer and GPT 4",
+      "creator": "Shreyas",
+      "contact": "",
+  },
+  {
+      "title": "Triboelectric Generator",
+      "description": "",
+      "creator": "",
       "contact": "",
   },
 ];
