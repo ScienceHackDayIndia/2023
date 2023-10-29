c4c0ba362b89f9616f47532b016e6d8a8c9419cf
Saptak S <saptak013@gmail.com>
2023-10-29 16:33:32 +0530

    Adds project page

---
+++ _data/projects.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+      "title": "Electromagnetic Clock",
+      "description": "Electromagnetic clock using relays, kuttypy and iron fillings",
+      "creator": "Sayan & Team",
+      "contact": "https://yudocaa.in",
+  },
+  {
+      "title": "Electromagnetic Clock",
+      "description": "Electromagnetic clock using relays, kuttypy and iron fillings",
+      "creator": "Sayan & Team",
+      "contact": "https://yudocaa.in",
+  },
+  {
+      "title": "Electromagnetic Clock",
+      "description": "Electromagnetic clock using relays, kuttypy and iron fillings",
+      "creator": "Sayan & Team",
+      "contact": "https://yudocaa.in",
+  },
+  {
+      "title": "Electromagnetic Clock",
+      "description": "Electromagnetic clock using relays, kuttypy and iron fillings",
+      "creator": "Sayan & Team",
+      "contact": "https://yudocaa.in",
+  },
+  {
+      "title": "Electromagnetic Clock",
+      "description": "Electromagnetic clock using relays, kuttypy and iron fillings",
+      "creator": "Sayan & Team",
+      "contact": "https://yudocaa.in",
+  },
+  {
+      "title": "Electromagnetic Clock",
+      "description": "Electromagnetic clock using relays, kuttypy and iron fillings",
+      "creator": "Sayan & Team",
+      "contact": "https://yudocaa.in",
+  },
+];
