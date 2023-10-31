7f578a90fb5fa779e2de934bcb04166a5fc740cc
Saptak S <saptak013@gmail.com>
2023-10-31 18:29:10 +0530

    Adds the individual team member contact url

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,14 @@
+const markdownIt = require("markdown-it")();
+
 module.exports = function(eleventyConfig) {
   // Output directory: _site
 
   // Copy `assets/` to `_site/assets`
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("CNAME");
+
+  // Custom filters
+  eleventyConfig.addAsyncFilter("contactHTMLize", async function(value) {
+    return markdownIt.renderInline(value);
+  });
 };
